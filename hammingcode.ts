2e252412e086721df200d5aa756ebd7c7d87f0c1
H3draut3r <H3draut3r@hotmail.de>
2022-03-27 21:45:39 +0200

    Update hammingcode.ts
    tested an number-generator with additional leading 0s for binary code

--- hammingcode.ts
+++ hammingcode.ts
@@ -5,15 +5,16 @@ export function sumOfPBits(_lengthOfDBits: number) {
     : ((Math.ceil(Math.log2(_lengthOfDBits * 2))) <= Math.ceil(Math.log2(1 + _lengthOfDBits + Math.ceil(Math.log2(_lengthOfDBits)))))
       ? Math.ceil(Math.log2(_lengthOfDBits) + 1)
       : Math.ceil(Math.log2(_lengthOfDBits))
-}
+};
 
-var test2: number = Math.round(Math.random() * (Math.pow(2, 26)))
-var bin_test2_data: string = test2.toString(2)
+var test2: number = Math.round((Math.random() * (Math.pow(2, (4 + Math.round(Math.random()*53))))));
+var bin_test2_data:string = test2.toString(2).padStart(
+  ((Math.pow(2, (sumOfPBits(test2.toString(2).length)))-(1+sumOfPBits(test2.toString(2).length))))
+  , "0");
 var bin_test2_parity:number = sumOfPBits(bin_test2_data.length)
 var test2_enc:Object = []
 
 
 
 
-
-console.log(test2, bin_test2_data, bin_test2_parity)
+console.log(test2, bin_test2_data, bin_test2_data.length, bin_test2_parity)
