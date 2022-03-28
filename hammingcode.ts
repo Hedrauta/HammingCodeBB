c0ee05061aeaa4259a4b89fdb77c8ce1bfe2b97b
Hedrauta <H3draut3r@hotmail.de>
2022-03-28 06:05:44 +0200

    Update hammingcode.ts

--- hammingcode.ts
+++ hammingcode.ts
@@ -1,5 +1,5 @@
 
-export function sumOfPBits(_lengthOfDBits: number) {
+function sumOfParityBits(_lengthOfDBits: number) {
   return (_lengthOfDBits <= 2 || _lengthOfDBits == 0)
     ? ((_lengthOfDBits == 0) ? null : _lengthOfDBits + 1)
     : ((Math.ceil(Math.log2(_lengthOfDBits * 2))) <= Math.ceil(Math.log2(1 + _lengthOfDBits + Math.ceil(Math.log2(_lengthOfDBits)))))
@@ -7,14 +7,15 @@ export function sumOfPBits(_lengthOfDBits: number) {
       : Math.ceil(Math.log2(_lengthOfDBits))
 };
 
-var test2: number = Math.round((Math.random() * (Math.pow(2, (4 + Math.round(Math.random()*53))))));
-var bin_test2_data:string = test2.toString(2).padStart(
-  ((Math.pow(2, (sumOfPBits(test2.toString(2).length)))-(1+sumOfPBits(test2.toString(2).length))))
-  , "0");
-var bin_test2_parity:number = sumOfPBits(bin_test2_data.length)
-var test2_enc:Object = []
+var test = {
+  value: Math.round((Math.random() * (Math.pow(2, (4 + Math.round(Math.random()*53)))))),
+  binary_value(){return this.value.toString(2)},
+  binary_pBits(){return sumOfParityBits(this.binary_value().length)},
+  data(){return this.binary_value().padStart(Math.pow(2, this.binary_pBits()) - (1+this.binary_pBits()), "0")}
+}
 
 
 
-
-console.log(test2, bin_test2_data, bin_test2_data.length, bin_test2_parity)
+console.log(
+  test.value, test.binary_value(), test.binary_pBits(), test.data()
+  )
